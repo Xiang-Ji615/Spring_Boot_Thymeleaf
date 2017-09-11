fb8b57dfacdc0fe248fbe68c8b617aa04d93fef6
jasper <jasper.ji@comactivity.com.au>
2017-09-11 14:54:30 +1000

    Add multiple viewsolver support

--- Spring_Thymeleaf/src/main/java/spring/thymeleaf/config/CustomThymeleafConfig.java
+++ Spring_Thymeleaf/src/main/java/spring/thymeleaf/config/CustomThymeleafConfig.java
@@ -1,12 +1,20 @@
 package main.java.spring.thymeleaf.config;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.expression.Sets;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import main.java.spring.thymeleaf.dialect.HelloWorldDialect;
 
@@ -25,12 +33,24 @@ public class CustomThymeleafConfig implements ApplicationContextAware{
 		yourTemplateResolver.setCheckExistence(true);
 		return yourTemplateResolver;
 	}
+	
+	@Bean
+	public ITemplateResolver templateResolver() {
+	    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+	    resolver.setPrefix("classpath:templates/");
+	    resolver.setSuffix(".html");
+	    resolver.setTemplateMode(TemplateMode.HTML);
+	    resolver.setCacheable(false);
+	    resolver.setOrder(1);
+	    return resolver;
+	}
 
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setEnableSpringELCompiler(true);
-		templateEngine.setTemplateResolver(yourTemplateResolver());
+		Set<ITemplateResolver> templateResolvers = new HashSet<>(Arrays.asList(templateResolver(), yourTemplateResolver()));
+		templateEngine.setTemplateResolvers(templateResolvers);
 		templateEngine.addDialect(new HelloWorldDialect());
 		return templateEngine;
 	}
